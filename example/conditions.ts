0a6543221e49613f523e26ecda1c33ca7b5109e4
Majo Richter <halsmaulmajo@coreunit.net>
2021-12-09 16:35:25 +0000

    Add: condition example

---
+++ example/conditions.ts
@@ -0,0 +1,29 @@
+import { SqlCondition } from "../src/index";
+
+const some: SqlCondition = [
+    "OR",
+    ["id", "==", 123],
+    [
+        "AND",
+        ["name", "==", "tester"],
+        ["age", "<", 32],
+        [
+            "OR",
+            ["active", true],
+            {
+                query: "'user'.'name' != $1 AND ('user'.age >= $3) = $2",
+                values: ["test", true, 123]
+            }
+        ]
+    ]
+]
+
+const test: SqlCondition = [
+    "OR",
+    [
+        "AND",
+        ["name", "tester"],
+    ["age", ">", 34],
+    ],
+    ["", ">", 34],
+]
